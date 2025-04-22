eb639a02bf50058e835ae2b52ac9ee1ea6f0de93
Albert <albertjhon.128@gmail.com>
2025-04-22 02:04:55 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/tasks')
+def list_tasks():
+    return render_template('tasks.html')
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
