221317f9c37acb5af7d03a23eaee1f549c22d190
Albert <albertjhon.128@gmail.com>
2025-04-22 20:10:41 -0400

    Templates y rutas completadas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -16,5 +16,22 @@ def signup():
 @app.route('/tasks')
 def list_tasks():
     return render_template('tasks.html')
+
+@app.route('/task/<int:id>')
+def view_task(id):
+    return render_template('task.html')
+
+@app.route('/task/create', methods=['GET', 'POST'])
+def create_task():
+    return render_template('create_task.html')
+
+@app.route('/task/edit/<int:id>', methods=['GET', 'POST'])
+def edit_task(id):
+    return render_template('edit_task.html')
+
+@app.route('/task/delete/<int:id>', methods=['POST'])
+def delete_task(id):
+    redirect(url_for('list_tasks'))
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
